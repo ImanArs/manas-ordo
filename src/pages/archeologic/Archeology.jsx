507a181a3cf0dc2e95442bc98a1bd271758f3925
ImanArs <imanbaevemaill.ru@gmail.com>
2023-01-07 13:49:34 +0600

    add: archeol

--- src/pages/archeologic/Archeology.jsx
+++ src/pages/archeologic/Archeology.jsx
@@ -15,7 +15,7 @@ const Archeology = () => {
         <div className="content">
           <div className={styles.archeology}>
             <div className={styles.video}>
-            <iframe width="400" height="200" src="https://www.youtube.com/embed/9rXoILcAPGA" title="Интервью директора археологического музея" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+            <iframe width="300" height="200" src="https://www.youtube.com/embed/9rXoILcAPGA" title="Интервью директора археологического музея" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
             </div>
             <div>
               <h1>{t("archeological.title")}</h1>
